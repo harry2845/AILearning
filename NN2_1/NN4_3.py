b9822c55663615fd02a120412e1e6b074ff71d8a
Dong Shen <shdon@microsoft.com>
2019-03-05 17:00:49 +0800

    add batch for nn4_3

--- NN2_1/NN4_3.py
+++ NN2_1/NN4_3.py
@@ -15,6 +15,13 @@ def ReadData():
 def ForwardCalculation(w, b, x):
     return np.dot(w, x) + b
 
+def BackPropagation1(x, y, z):
+    m = x.shape[1]
+    dz = z - y
+    db = sum(sum(dz)).reshape(1, -1) / m
+    dw = np.dot(dz, np.transpose(x)) / m
+    return dw, db
+
 def BackPropagation(x, y, z):
     dz = z - y
     db = dz
@@ -43,6 +50,20 @@ def ShowResult(X, Y, w, b, iteration):
     print(iteration)
     print(w, b)
 
+def ShowResult1(X, Y, w, b, iteration):
+    plt.plot(X, Y, 'b.')
+    PX = np.linspace(0, 1, 10)
+    w = sum(w)
+    b = sum(b)
+    PZ = w * PX + b
+    plt.plot(PX, PZ, 'r')
+    plt.title("Air Conditioner Power")
+    plt.xlabel("Number of Servers(K)")
+    plt.ylabel("Power of Air Conditioner(KW)")
+    plt.show()
+    print(iteration)
+    print(w, b)
+
 if __name__ == '__main__':
     X, Y = ReadData()
 
@@ -56,6 +77,18 @@ if __name__ == '__main__':
         w, b = UpdateWeights(w, b, dw, db, eta)
 
     ShowResult(X, Y, w, b, 1)
+
+    result = ForwardCalculation(w, b, 0.346)
+    print("result = ", result)
+
+    iter = 500
+    for i in range(iter):
+        #x, y = GetSample(X, Y, i)
+        z = ForwardCalculation(w, b, X)
+        dw, db = BackPropagation1(X, Y, z)
+        w, b = UpdateWeights(w, b, dw, db, eta)
+
+    ShowResult1(X, Y, w, b, iter)
 
     result = ForwardCalculation(w, b, 0.346)
     print("result = ", result)
